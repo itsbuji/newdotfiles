4dd787f7981347ea52a859f47a35bbe10d0fbdf6
Akshay KM <akshay@aventusinformatics.in>
2026-06-09 14:00:05 +0530

    batman

---
+++ .config/nvim/init.lua
@@ -0,0 +1,291 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.opt.termguicolors = true
+
+vim.o.relativenumber = true
+vim.o.number = true
+
+vim.opt.undodir = vim.fn.expand("~/.vim/undodir")
+vim.opt.undofile = true
+
+vim.opt.directory = vim.fn.stdpath("state") .. "/swap//"
+vim.opt.updatecount = 100
+
+vim.opt.swapfile = true
+vim.opt.backup = false
+
+vim.o.splitright = true
+vim.opt.scrolloff = 8
+vim.o.incsearch = true
+vim.o.ignorecase = false
+vim.o.smartcase = true
+
+vim.opt.wrap = false
+vim.opt.colorcolumn = "80"
+
+vim.opt.cursorline = true
+
+vim.opt.spelllang = "en_us"
+vim.opt.spell = true
+
+vim.opt.expandtab = false
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 8
+vim.opt.smartindent = true
+
+vim.opt.statusline = "%f%h%m%r%w%=  %y %l,%c | %P | %{strftime('%H:%M')}"
+
+vim.opt.shortmess:append("c")
+
+vim.opt.path = { ".", "**" }
+vim.opt.wildignore:append({
+	"**/node_modules/**",
+	"**/dist/**",
+	"**/build/**",
+	"**/.git/**",
+	"**/.next/**",
+	"**/.turbo/**",
+	"**/.cache/**",
+	"**/coverage/**",
+	"**/out/**",
+})
+
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the top window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the bottom window" })
+
+vim.keymap.set("n", "n", "nzzzv", { desc = "center after next search match" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "center after previous search match" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "center after half page down" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "center after half page up" })
+
+vim.keymap.set("n", "<leader>fd", ":find ", { desc = "Find file" })
+vim.keymap.set("n", "<leader>g", ":grep ", { desc = "Grep" })
+vim.keymap.set("n", "<leader>l", ":ls<CR>", { desc = "List buffers" })
+
+local highlightGroup = vim.api.nvim_create_augroup("highlight-yank", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Hightlight when yanking (copying) text",
+	group = highlightGroup,
+	callback = function()
+		vim.highlight.on_yank({ timeout = 80 })
+	end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	callback = function()
+		vim.opt_local.path = { ".", "**" }
+	end,
+})
+
+require("vim._core.ui2").enable({ enable = true })
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "#16181d" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1c1f26" })
+vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#3d4250", bg = "#1c1f26" })
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "typescriptreact" },
+	callback = function()
+		vim.bo.omnifunc = "javascriptcomplete#CompleteJS"
+	end,
+})
+
+vim.pack.add({
+	{
+		src = "https://github.com/ibhagwan/fzf-lua",
+	},
+	{
+		src = "https://github.com/nvim-treesitter/nvim-treesitter",
+	},
+	{ src = "https://github.com/stevearc/conform.nvim" },
+	{
+		src = "https://github.com/kylechui/nvim-surround",
+		name = "Nvim Surround",
+	},
+	{ src = "https://github.com/mason-org/mason.nvim", name = "Mason" },
+	{ src = "https://github.com/Saghen/blink.cmp" },
+	{ src = "https://github.com/Saghen/blink.lib" },
+})
+
+local conform = require("conform")
+
+conform.setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		javascript = { "prettier" },
+		typescript = { "prettier" },
+		javascriptreact = { "prettier" },
+		typescriptreact = { "prettier" },
+		css = { "prettier" },
+		html = { "prettier" },
+		json = { "prettier" },
+		yaml = { "prettier" },
+		markdown = { "prettier" },
+		go = { "goimports", "gofmt" },
+	},
+
+	format_on_save = {
+		lsp_fallback = false,
+		async = false,
+		timeout_ms = 1000,
+	},
+})
+
+local blink = require("blink.cmp")
+blink.setup({
+	completion = {
+		trigger = {
+			show_on_insert = true,
+			show_on_keyword = true,
+			show_on_trigger_character = true,
+			show_on_backspace = true,
+			show_on_backspace_in_keyword = true,
+			show_on_backspace_after_accept = true,
+			show_on_backspace_after_insert_enter = true,
+			show_on_accept_on_trigger_character = true,
+			show_on_insert_on_trigger_character = true,
+		},
+		menu = {
+			border = "rounded",
+			winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder",
+		},
+		documentation = {
+			window = { border = "rounded" },
+		},
+	},
+	sources = {
+		default = { "lsp", "buffer" },
+	},
+	keymap = {
+		preset = "enter",
+		["<C-Space>"] = { "show", "hide" },
+		["<C-n>"] = { "select_next" },
+		["<C-p>"] = { "select_prev" },
+		["<C-y>"] = { "select_and_accept" },
+		["<C-e>"] = { "hide" },
+		["<C-b>"] = { "scroll_documentation_up" },
+		["<C-f>"] = { "scroll_documentation_down" },
+	},
+})
+
+vim.lsp.config("*", {
+	root_markers = { ".git" },
+})
+
+vim.lsp.config("lua_ls", {
+	cmd = { "lua-language-server" },
+	filetypes = { "lua" },
+})
+
+vim.lsp.config("gopls", {
+	cmd = { "gopls" },
+	filetypes = { "go", "gomod", "gowork", "gotmpl" },
+})
+
+vim.lsp.config("vtsls", {
+	cmd = { "vtsls", "--stdio" },
+	filetypes = {
+		"javascript",
+		"javascriptreact",
+		"typescript",
+		"typescriptreact",
+	},
+})
+
+vim.lsp.config("marksman", {
+	cmd = { "marksman" },
+	filetypes = { "markdown" },
+})
+
+vim.lsp.enable({ "lua_ls", "vtsls", "gopls", "marksman" })
+
+local orig_hover = vim.lsp.buf.hover
+vim.lsp.buf.hover = function(config)
+	config = vim.tbl_deep_extend("force", config or {}, { border = "rounded" })
+	orig_hover(config)
+end
+
+local orig_sig = vim.lsp.buf.signature_help
+vim.lsp.buf.signature_help = function(config)
+	config = vim.tbl_deep_extend("force", config or {}, { border = "rounded" })
+	orig_sig(config)
+end
+
+vim.diagnostic.config({ float = { border = "rounded" } })
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UserLSPConfig", {}),
+	callback = function(ev)
+		local opts = { buffer = ev.buf, silent = true }
+		local keymap = vim.keymap
+
+		vim.keymap.set("n", "grd", vim.diagnostic.open_float, { desc = "Open diagnostic float" })
+
+		opts.desc = "Add Diagnostic to local list"
+		keymap.set("n", "gql", vim.diagnostic.setqflist, opts)
+
+		vim.keymap.set("n", "]d", function()
+			vim.diagnostic.goto_next()
+			vim.diagnostic.open_float()
+		end, { desc = "Next diagnostic with float" })
+		vim.keymap.set("n", "[d", function()
+			vim.diagnostic.goto_prev()
+			vim.diagnostic.open_float()
+		end, { desc = "Prev diagnostic with float" })
+	end,
+})
+
+local ms = require("mason")
+ms.setup()
+
+local fzf = require("fzf-lua")
+
+fzf.setup({
+	winopts = {
+		height = 0.80,
+		width = 0.70,
+		row = 0.35,
+		col = 0.50,
+		border = "rounded",
+		preview = {
+			layout = "horizontal",
+			horizontal = "down:60%",
+		},
+	},
+	hls = { border = "FloatBorder" },
+})
+
+require("fzf-lua").register_ui_select({
+	silent = true,
+})
+
+vim.keymap.set({ "n", "v", "i" }, "<C-x><C-f>", function()
+	FzfLua.complete_path()
+end, { silent = true, desc = "Fuzzy complete path" })
+
+vim.keymap.set("n", "<C-p>", function()
+	FzfLua.files()
+end, { silent = true, desc = "Fuzzy complete path" })
+
+vim.keymap.set("n", "g/", function()
+	FzfLua.live_grep_native()
+end, { silent = true, desc = "Fuzzy complete path" })
+
+vim.keymap.set("n", "gS", function()
+	FzfLua.lsp_live_workspace_symbols()
+end, { silent = true, desc = "Fuzzy complete path" })
+
+vim.keymap.set("n", "grr", function()
+	FzfLua.lsp_references()
+end, { silent = true, desc = "Fuzzy complete path" })
+
+vim.keymap.set("n", "gra", function()
+	FzfLua.lsp_code_actions()
+end, { silent = true, desc = "Fuzzy complete path" })
+
+vim.keymap.set("n", "gO", function()
+	FzfLua.lsp_document_symbols()
+end, { silent = true, desc = "Fuzzy complete path" })
---
+++ .zshrc
@@ -0,0 +1,50 @@
+export ZSH="$HOME/.oh-my-zsh"
+ZSH_THEME="robbyrussell"
+
+plugins=(git zsh-autosuggestions fast-syntax-highlighting)
+
+source $ZSH/oh-my-zsh.sh
+#
+# # eval "$(zoxide init zsh)"
+
+if [[ -n $SSH_CONNECTION ]]; then
+  export EDITOR='vim'
+else
+  export EDITOR='nvim'
+fi
+
+
+export PATH=$PATH:/usr/local/go/bin
+export PATH="$HOME/.local/share/nvim/mason/bin:$PATH"
+
+# opencode
+export PATH=/home/ak/.opencode/bin:$PATH
+
+
+fj() {
+  local job
+  job=$(jobs -s | fzf | awk '{print $1}' | tr -d '[]')
+  [ -n "$job" ] && fg %"$job"
+}
+
+
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+export PATH="$PATH:/opt/nvim-linux-x86_64/bin"
+
+export PATH="$HOME/.local/bin:$PATH"
+export PATH="$HOME/.odin:$PATH"
+
+source <(fzf --zsh)
+
+bindkey -v
+
+# bun completions
+[ -s "/home/ak/.bun/_bun" ] && source "/home/ak/.bun/_bun"
+
+# bun
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
